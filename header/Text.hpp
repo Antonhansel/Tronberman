4c66aa0b61484f208b6d382e00d1da35b24a86dd
Antonin Ribeaud <antonin.ribeaud@epitech.eu>
2014-05-02 18:43:34 +0200

    Commit

---
+++ header/Text.hpp
@@ -0,0 +1,25 @@
+/*
+** Text.hpp for Text in /home/apollo/rendu/cpp_bomberman/header
+**
+** Made by Antonin Ribeaud
+** Login   <ribeau_a@epitech.net>
+**
+** Started on  Fri May  02 10:46:24 2014 Antonin Ribeaud
+** Last update Fri May  02 10:46:24 2014 Antonin Ribeaud
+*/
+
+#ifndef _TEXT_HPP_
+# define _TEXT_HPP_
+
+#include "Core.hpp"
+
+class Text
+{
+public:
+	Text();
+	~Text();
+private:
+	
+};
+
+#endif /*!TEXT_HPP*/
---
+++ srcs/Text.cpp
@@ -0,0 +1,21 @@
+/*
+** Text.cpp for Text in /home/apollo/rendu/cpp_bomberman/srcs
+**
+** Made by Antonin Ribeaud
+** Login   <ribeau_a@epitech.net>
+**
+** Started on  Fri May  02 10:46:08 2014 Antonin Ribeaud
+** Last update Fri May  02 10:46:08 2014 Antonin Ribeaud
+*/
+
+#include "Text.hpp"
+
+Text::Text()
+{
+
+}
+
+Text::~Text()
+{
+
+}
