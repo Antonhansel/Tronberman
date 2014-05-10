5aa430c0241da583e6d9f2f01a8dcdbf4febca16
Antonin Ribeaud <antonin.ribeaud@epitech.eu>
2014-05-10 17:25:29 +0200

    commit

--- loader.sh
+++
@@ -1,2 +0,0 @@
-eval 'sudo apt-get install g++ build-essential libpthread-stubs0-dev libgl1-mesa-dev libx11-dev libxrandr-dev libfreetype6-dev libglew-dev libjpeg-dev libsndfile1-dev libopenal-dev'
-eval 'export LD_LIBRARY_PATH=bomberlib/'
